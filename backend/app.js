30e0ee99f311c38e020f5f309139582751b87b46
Jonathan DIki <jonathandiki@outlook.com>
2024-06-14 01:49:51 +0200

    Update 13/05/2024

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,6 @@ const express = require('express');
     
 const app = express();
 app.use((req, res) => {
-   res.json({ message: 'Votrek requête a bien été reçue !' }); 
+   res.json({ message: 'Votre requête a bien été reçue ghkg!!' }); 
 });
 module.exports = app;
